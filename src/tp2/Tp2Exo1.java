5f1eb70f59bcafc0bd2f5486ee2f7dda38f83bb9
MAR.AVITABI <MAR.AVITABI@s124-10.pedago.mermoz.edu>
2019-11-04 15:08:52 +0100

    Tp1Exo1 et Tp2Exo1 fait !

--- src/tp2/Tp2Exo1.java
+++ src/tp2/Tp2Exo1.java
@@ -1,9 +1,14 @@
 package tp2;
-
+import java.util.Scanner;
 public class Tp2Exo1 {
 
-	public static void main(String[] args) {
-		System.out.print("TP2");
-	}
+public static void main(String[]args){
+		
+		Scanner lecture_clavier =new Scanner(System.in);
+		int valeur;
+		System.out.print("Saisir un entier : ");
+		valeur =lecture_clavier.nextInt();System.out.print("Entier saisi = "+valeur);
+		lecture_clavier.close();
+		}
 
 }
