b24c1ba5ca6bfc403dbbc366873e135d64fd7509
MAR.AVITABI <MAR.AVITABI@s124-10.pedago.mermoz.edu>
2019-11-04 16:02:30 +0100

    Correction de lexo 1 du tp2

--- src/tp2/Tp2Exo1.java
+++ src/tp2/Tp2Exo1.java
@@ -5,9 +5,16 @@ public class Tp2Exo1 {
 public static void main(String[]args){
 		
 		Scanner lecture_clavier =new Scanner(System.in);
-		int valeur;
-		System.out.print("Saisir un entier : ");
-		valeur =lecture_clavier.nextInt();System.out.print("Entier saisi = "+valeur);
+		int valeur1;
+		int valeur2;
+		
+		System.out.println("Saisir un entier : ");
+		valeur1 =lecture_clavier.nextInt();
+		//System.out.print("Entier saisi = "+valeur);
+		System.out.println("Saisir une autre entier : ");
+		valeur2 =lecture_clavier.nextInt();
+		System.out.print("La somme des deux valeurs est  = "+(valeur1+valeur2));
+		System.out.print("Le produit des deux valeurs est  = "+valeur1*valeur2);
 		lecture_clavier.close();
 		}
 
