46e95e9199b3b767bc515e38951fd852f98046e3
MAR.AVITABI <MAR.AVITABI@s124-10.pedago.mermoz.edu>
2019-11-06 09:21:35 +0100

    Finition de tp3 EXO1

--- src/tp3/Tp3Exo1.java
+++ src/tp3/Tp3Exo1.java
@@ -20,38 +20,38 @@ public class Tp3Exo1 {
 		char texte;
 		System.out.println("Saisir un byte entre -128 et 127 : ");
 		bytes =lecture_clavier.nextByte();
-		System.out.print("bytes = "+bytes );
+		System.out.println("bytes = "+bytes );
 		
 		//short
 		System.out.println("Saisir un short entre -32768 et 32767 : ");
 		shorts =lecture_clavier.nextShort();
-		System.out.print("short = "+ shorts);
+		System.out.println("short = "+ shorts);
 		
 		//int
 		System.out.println("Saisir un int entre -2147483 648 et 2147483 648 : ");
 		ints =lecture_clavier.nextInt();
-		System.out.print("short = "+ ints);
+		System.out.println("short = "+ ints);
 		
 		//long
 		System.out.println("Saisir un long entre -9223372036854775 808 et 9223372036854775 808 : ");
 		longs =lecture_clavier.nextLong();
-		System.out.print("long = "+ longs);
+		System.out.println("long = "+ longs);
 		//float
 		System.out.println("Saisir un long entre +/-1.4e-45 et +/-3.4e38 : ");
 		floats =lecture_clavier.nextFloat();
-		System.out.print("long = "+ floats);
+		System.out.println("long = "+ floats);
 		//double
 		System.out.println("Saisir un long entre +/. 4.940e-324 et +/-1.797e308 : ");
 		doubles =lecture_clavier.nextDouble();
-		System.out.print("double = "+ doubles);
+		System.out.println("double = "+ doubles);
 		//boolean
 		System.out.println("Saisir une valeur logique : true ou false: ");
 		boolaens =lecture_clavier.nextBoolean();
-		System.out.print("boolean = "+ boolaens);
+		System.out.println("boolean = "+ boolaens);
 		//Varchar
 		System.out.println("Saisir une chaine de caractère");
 		texte =lecture_clavier.next().charAt(0);
-		System.out.print("Char = "+ texte);
+		System.out.println("Char = "+ texte);
 		
 		
 		lecture_clavier.close();
