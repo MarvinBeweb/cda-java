05edecd1a3ffed05bf8dd8ac9d91fc67f9002045
MAR.AVITABI <MAR.AVITABI@s124-10.pedago.mermoz.edu>
2019-11-06 15:23:00 +0100

    Tp3Exo4

--- src/tp3/Tp3Exo4.java
+++ src/tp3/Tp3Exo4.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Tp3Exo4 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 			  Scanner input = new Scanner(System.in);
 		      System.out.println("Entre une valeur en seconde: ");
 		      int n = input.nextInt();
