8c6acb3971b5f1d94c052f81b88902b985a7d285
MAR.AVITABI <MAR.AVITABI@s124-10.pedago.mermoz.edu>
2019-11-06 10:40:03 +0100

    Tp3Exo2

---
+++ src/tp3/tp3Exo2.java
@@ -0,0 +1,21 @@
+package tp3;
+
+public class tp3Exo2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n = 10, p=4;
+		long q = 2111000222;
+		float x = 1.75f;
+		
+		long b = n + q ;//2111000232
+	    float c = n + x ;//11.75
+	    int d = n % -p * n;//20
+	    int e = -n % p * n ;//20
+	    
+		
+		System.out.print(b);
+		
+	}
+
+}
