b9c270e623d355a8b25f7f6bb19a0356baa4a5c7
mar.avitabi <mar.avitabi@s124-10.pedago.mermoz.edu>
2019-11-18 16:14:41 +0100

    tp4Exo6

---
+++ src/tp4/Tp4Exo6.java
@@ -0,0 +1,34 @@
+package tp4;
+
+import java.util.Scanner;
+
+public class Tp4Exo6 {
+
+	public static void main(String[] args) {
+        int impaire = 1;
+        int sommeImpaire = 0;
+		Scanner clavier = new Scanner(System.in);
+		System.out.println("Donnez une valeur comprise entre 1 et 100 inclus");
+		int nb = clavier.nextInt();
+		
+		
+		while (nb < 1 || nb > 100) {
+			System.out.println(
+					"Non!. Donnez une valeur comprise entre 1 et 100 inclus SVP");	
+					nb = clavier.nextInt();
+		}
+		for (int i = 0; i < nb; i++) {
+			System.out.println(impaire);
+			sommeImpaire += impaire;
+			impaire += 2;
+		}
+		System.out.println(sommeImpaire);
+
+        clavier.close();
+        
+	
+	}
+}
+
+	
+
